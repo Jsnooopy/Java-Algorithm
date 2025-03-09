128e1185b882eb7507462bcdce176ab8369a7cda
DearJ <108777654+Jsnooopy@users.noreply.github.com>
2025-03-09 12:17:54 +0900

    Add files via upload

---
+++ P11050.java
@@ -0,0 +1,26 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class P11050 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        long result = factorial(n) / (factorial(k) * factorial(n - k));
+
+        System.out.println(result);
+    }
+
+    private static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+}
